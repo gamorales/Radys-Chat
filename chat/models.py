3de841112950366fe81af7522ce999f75a35c931
RadicalEd <guilleccep@gmail.com>
2019-06-21 19:58:21 -0500

    Users Model added

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Usuarios(models.Model):
+    email = models.EmailField(max_length=254)
+    usuario = models.CharField(max_length=15)
+    password = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.usuario
+
+
